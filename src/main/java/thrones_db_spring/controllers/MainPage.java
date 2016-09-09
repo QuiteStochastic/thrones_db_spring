b55d447f285377245a1f9def32c509d4356c156d
oliverlee <Lee_Oliver@bah.com>
2016-09-09 17:56:15 -0400

    trying to use thyme leaf, not working for some reason even though i'm matching online examples.  might try jsp

--- src/main/java/thrones_db_spring/controllers/MainPage.java
+++ src/main/java/thrones_db_spring/controllers/MainPage.java
@@ -1,23 +1,25 @@
 package thrones_db_spring.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by oliverlee
  */
-//@RestController
+@Controller
 public class MainPage {
 
 	@RequestMapping(path="/",method = RequestMethod.GET)
-	public String mainPage(){
+	public String mainPage(Model model){
 
 		System.out.println("hit main page");
 
 
-
-		return "<!DOCTYPE HTML>\n" +
+		model.addAttribute("test", "hello spring");
+		return "mainpage";
+		/*return "<!DOCTYPE HTML>\n" +
 				"<html>" +
 				"<head>\n" +
 				"    <title>Thrones DB Hello World</title>\n" +
@@ -25,7 +27,7 @@ public class MainPage {
 				"<body>\n" +
 				"    <p>Hello spring boot</p>\n" +
 				"</body>\n" +
-				"</html>";
+				"</html>";*/
 	}
 
 
