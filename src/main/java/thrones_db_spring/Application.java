51b8b9db341949120157a91e9334e5ee268b6c1f
oliverlee <Lee_Oliver@bah.com>
2016-09-23 14:59:37 -0400

    get rid of that silly flyway script nonsense for the db, don't need that anymore

--- src/main/java/thrones_db_spring/Application.java
+++ src/main/java/thrones_db_spring/Application.java
@@ -1,12 +1,8 @@
 package thrones_db_spring;
 
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import javax.annotation.PostConstruct;
 
 /**
  * Created by oliverlee
@@ -20,39 +16,5 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 
 	}
-
-
-
-/*	@Value("${schema}")
-	private String schemaName;
-
-	@Value("${hibernate.connection.url}")
-	private String db_url;
-
-	@Value("${hibernate.connection.username}")
-	private String db_UN;
-
-	@Value("${hibernate.connection.password}")
-	private String db_PW;
-
-
-	@PostConstruct
-	public void runFlywayScripts() {
-
-		System.out.println("INITIALIZING db");
-		System.out.println("db_fullURL: "+db_url);
-		System.out.println("db_User: "+db_UN);
-		System.out.println("db_PW: "+db_PW);
-		System.out.println("scheme: "+schemaName);
-		System.out.println("setting up flyway");
-
-		Flyway flyway = new Flyway();
-		flyway.setDataSource(db_url, db_UN, db_PW);
-		flyway.setSchemas(schemaName);
-
-		System.out.println("executing flyway migrate\n");
-		flyway.migrate();
-
-	}*/
 
 }
