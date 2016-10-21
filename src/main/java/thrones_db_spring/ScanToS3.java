8513a3c755bd8d1429ca795b7c7f5367dd109400
oliverlee <Lee_Oliver@bah.com>
2016-10-20 22:00:30 -0400

    scan to s3 is multithreaded and now grabs all the html pages, but not images and css files and what not, also is untested

--- src/main/java/thrones_db_spring/ScanToS3.java
+++ src/main/java/thrones_db_spring/ScanToS3.java
@@ -1,9 +1,17 @@
 package thrones_db_spring;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 /**
  * Created by oliverl1
@@ -17,56 +25,70 @@ public class ScanToS3 {
     private static final int numEvents=10;
     private static final int numEpisodes=40;
 
-    private static final String domain="http://localhost";
+    private static final String siteDomain ="http://localhost";
 
+
+	private static final String bucketName     = "valar-morghulis.org";
+
+	private static AmazonS3 s3client;
 
 
     public static void main(String[] args) {
 
-        RestTemplate restTemplate = new RestTemplate();
+		String accessKeyId;
+		String secretKeyId;
+
+		try {
+			Scanner inFile = new Scanner(new FileReader("./dev_user.txt"));
+
+			//skip the first line
+			inFile.next();
+
+			String[] line=inFile.next().split(",");
+
+			//in my current file format, the needed fields are the 2nd and 3rd entry in the csv file
+			accessKeyId=line[1];
+			secretKeyId=line[2];
 
 
-        String mainPage=restTemplate.getForObject(domain,String.class);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
 
-        List<String> topLevelPages=new ArrayList<String>();
-        topLevelPages.add(restTemplate.getForObject(domain+"/characters",String.class));
-        topLevelPages.add(restTemplate.getForObject(domain+"/organizations",String.class));
-        topLevelPages.add(restTemplate.getForObject(domain+"/locations",String.class));
-        topLevelPages.add(restTemplate.getForObject(domain+"/events",String.class));
-        topLevelPages.add(restTemplate.getForObject(domain+"/episodes",String.class));
-        topLevelPages.add(restTemplate.getForObject(domain+"/about",String.class));
+		BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKeyId, secretKeyId);
+		s3client = AmazonS3ClientBuilder.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+				.build();
 
-
-
-        List<String> characterPages=new ArrayList<String>();
-        for(int n=1;n<=numCharacters;n++){
-            characterPages.add(restTemplate.getForObject(domain+"/characters/"+n,String.class));
-        }
-
-
-        List<String> organizationPages=new ArrayList<String>();
-        for(int n=1;n<=numOrganizations;n++){
-            organizationPages.add(restTemplate.getForObject(domain+"/organizations/"+n,String.class));
-        }
+		RestTemplate restTemplate = new RestTemplate();
 
 
 
-        List<String> locationPages=new ArrayList<String>();
-        for(int n=1;n<=numLocations;n++){
-            locationPages.add(restTemplate.getForObject(domain+"/locations/"+n,String.class));
-        }
+
+		if(!s3client.doesBucketExist(bucketName)){
+			s3client.createBucket(bucketName);
+		}
 
 
-        List<String> eventPages=new ArrayList<String>();
-        for(int n=1;n<=numEvents;n++){
-            eventPages.add(restTemplate.getForObject(domain+"/events/"+n,String.class));
-        }
+		uploadToS3("main",restTemplate.getForObject(siteDomain,String.class));
+
+		uploadToS3("characters",restTemplate.getForObject(siteDomain +"/characters",String.class));
+		uploadToS3("organizations",restTemplate.getForObject(siteDomain +"/organizations",String.class));
+		uploadToS3("locations",restTemplate.getForObject(siteDomain +"/locations",String.class));
+		uploadToS3("events",restTemplate.getForObject(siteDomain +"/events",String.class));
+		uploadToS3("episodes",restTemplate.getForObject(siteDomain +"/episodes",String.class));
+		uploadToS3("about",restTemplate.getForObject(siteDomain +"/about",String.class));
 
 
-        List<String> episodesPages=new ArrayList<String>();
-        for(int n=1;n<=numEpisodes;n++){
-            episodesPages.add(restTemplate.getForObject(domain+"/episodes/"+n,String.class));
-        }
+		new Thread(() -> fetchPageAndUpload("characters",numCharacters)).start();
+		new Thread(() -> fetchPageAndUpload("organizations",numOrganizations)).start();
+		new Thread(() -> fetchPageAndUpload("locations",numLocations)).start();
+		new Thread(() -> fetchPageAndUpload("events",numEvents)).start();
+		new Thread(() -> fetchPageAndUpload("episodes",numEpisodes)).start();
+
+
+
 
 
 
@@ -74,4 +96,53 @@ public class ScanToS3 {
 
 
     }
+
+
+	private static void fetchPageAndUpload(String path,int numPages) {
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		for(int n=1;n<=numPages;n++){
+
+			final String keyName=path+"/"+ n;
+
+			new Thread(()->uploadToS3(keyName,restTemplate.getForObject(siteDomain +"/"+keyName,String.class))).start();
+
+		}
+
+	}
+
+
+
+
+
+	private static void uploadToS3(String keyName,String uploadObject){
+
+
+		try {
+			System.out.println("Uploading a new object to S3 from a file\n");
+
+			s3client.putObject(new PutObjectRequest(bucketName, keyName, uploadObject));
+
+		} catch (AmazonServiceException ase) {
+			System.out.println("Caught an AmazonServiceException, which " +
+					"means your request made it " +
+					"to Amazon S3, but was rejected with an error response" +
+					" for some reason.");
+			System.out.println("Error Message:    " + ase.getMessage());
+			System.out.println("HTTP Status Code: " + ase.getStatusCode());
+			System.out.println("AWS Error Code:   " + ase.getErrorCode());
+			System.out.println("Error Type:       " + ase.getErrorType());
+			System.out.println("Request ID:       " + ase.getRequestId());
+		} catch (AmazonClientException ace) {
+			System.out.println("Caught an AmazonClientException, which " +
+					"means the client encountered " +
+					"an internal error while trying to " +
+					"communicate with S3, " +
+					"such as not being able to access the network.");
+			System.out.println("Error Message: " + ace.getMessage());
+		}
+	}
+
+
 }
