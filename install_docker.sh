cfe97256c2716c81d9c1c20272fd61990cc0727e
QuiteStochastic <quitestochastic@gmail.com>
2016-12-19 14:54:43 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -3,7 +3,7 @@
 
 #docker installation, just a general script for use on ubuntu, not particular to this project
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get upgrade -y
 
 sudo apt-get install docker.io -y
@@ -11,3 +11,5 @@ sudo service docker start
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
